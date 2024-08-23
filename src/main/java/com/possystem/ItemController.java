1e3be6787923b23c3ef41d63d37b4ab9b4a5c083
pathumck <ckpathumck@gmail.com>
2024-08-23 19:45:24 +0530

    create item servlet

---
+++ src/main/java/com/possystem/ItemController.java
@@ -0,0 +1,8 @@
+package com.possystem;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/item")
+public class ItemController extends HttpServlet {
+}
