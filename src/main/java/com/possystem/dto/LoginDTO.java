bcbe862d8e525f2523255d7ece3e5a035352dfe5
pathumck <ckpathumck@gmail.com>
2024-08-25 00:32:58 +0530

    create LoginDTO

---
+++ src/main/java/com/possystem/dto/LoginDTO.java
@@ -0,0 +1,13 @@
+package com.possystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LoginDTO {
+    private String name;
+    private String password;
+}
