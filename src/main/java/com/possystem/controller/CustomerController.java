39cc0a26f60d59437f679a4a38e7de2a7d331644
pathumck <ckpathumck@gmail.com>
2024-08-24 13:15:30 +0530

    create database configuration using JNDI

--- src/main/java/com/possystem/controller/CustomerController.java
+++ src/main/java/com/possystem/controller/CustomerController.java
@@ -1,8 +1,27 @@
 package com.possystem.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @WebServlet(urlPatterns = "/customer", loadOnStartup = 2)
 public class CustomerController extends HttpServlet {
+    Connection connection = null;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/pathumpossystem");
+            this.connection = pool.getConnection();
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
