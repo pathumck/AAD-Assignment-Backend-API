f5021fdde0deeda1b5f689714f3116fde9d27f7c
pathumck <ckpathumck@gmail.com>
2024-08-30 18:55:48 +0530

    create Order entity

---
+++ src/main/java/com/possystem/entity/Order.java
@@ -0,0 +1,17 @@
+package com.possystem.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Order {
+    private String id;
+    private Date date;
+    private String customerId;
+    private Double total;
+}
